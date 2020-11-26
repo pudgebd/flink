8f0c271798650b0721fe619c253365b3a3f3481a
pudgebd <pudgebd>
2020-11-26 19:24:46 +0800

    +++

--- src/main/java/pers/pudgebd/flink/java/joinAndWindow/JoinAndWindow05.java
+++ src/main/java/pers/pudgebd/flink/java/joinAndWindow/JoinAndWindow05.java
@@ -1,10 +1,11 @@
 package pers.pudgebd.flink.java.joinAndWindow;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.api.common.functions.CoGroupFunction;
 import org.apache.flink.api.common.functions.FlatJoinFunction;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
@@ -18,7 +19,7 @@ import pers.pudgebd.flink.java.func.AlertSelfBuySellUdtaf;
 
 import static pers.pudgebd.flink.java.joinAndWindow.JoinAndWindow01.createSth;
 
-public class JoinAndWindow04 {
+public class JoinAndWindow05 {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
@@ -30,28 +31,34 @@ public class JoinAndWindow04 {
         tableEnv.registerFunction("alertSelfBuySellUdtaf", new AlertSelfBuySellUdtaf());
 
         Table kafka_stock_order = tableEnv.from("kafka_stock_order");
-        DataStream<Row> orderDs = tableEnv.toAppendStream(kafka_stock_order, Row.class);
+        DataStream<Row> ksoDs = tableEnv.toAppendStream(kafka_stock_order, Row.class);
         Table kafka_stock_order_confirm = tableEnv.from("kafka_stock_order_confirm");
-        DataStream<Row> confirmDs = tableEnv.toAppendStream(kafka_stock_order_confirm, Row.class);
+        DataStream<Row> ksocDs = tableEnv.toAppendStream(kafka_stock_order_confirm, Row.class);
 
-        KeyedStream<Row, Object> orderKs = orderDs.keyBy(new KeySelector<Row, Object>() {
-            @Override
-            public Object getKey(Row value) throws Exception {
-                Object order_no = value.getField(2);
-                return order_no;
-            }
-        });
-
-        KeyedStream<Row, Object> confirmKs = confirmDs.keyBy(new KeySelector<Row, Object>() {
-            @Override
-            public Object getKey(Row value) throws Exception {
-                Object order_no = value.getField(3);
-                return order_no;
-            }
-        });
-
-        orderKs.connect(confirmKs)
-        ;
+        ksoDs.coGroup(ksocDs)
+                .where(new KeySelector<Row, Object>() {
+                    @Override
+                    public Object getKey(Row value) throws Exception {
+                        Object order_no = value.getField(2);
+                        return order_no;
+                    }
+                })
+                .equalTo(new KeySelector<Row, Object>() {
+                    @Override
+                    public Object getKey(Row value) throws Exception {
+                        Object order_no = value.getField(3);
+                        return order_no;
+                    }
+                })
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(3)))
+                .apply(new CoGroupFunction<Row, Row, Object>() {
+                    @Override
+                    public void coGroup(Iterable<Row> first, Iterable<Row> second, Collector<Object> out) throws Exception {
+//                        out.collect();
+                    }
+                })
+        .print();
+        streamEnv.execute("a");
     }
 
 }
