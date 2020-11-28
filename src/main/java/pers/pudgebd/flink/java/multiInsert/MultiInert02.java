31deb23f9558d1b3580a06865229067b2493f021
pudgebd <pudgebd>
2020-11-28 16:15:21 +0800

    +++

--- src/main/java/pers/pudgebd/flink/java/multiInsert/MultiInert02.java
+++ src/main/java/pers/pudgebd/flink/java/multiInsert/MultiInert02.java
@@ -9,14 +9,65 @@ import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.types.Row;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
 
+import static org.apache.flink.table.api.Expressions.$;
 import static pers.pudgebd.flink.java.joinAndWindow.JoinAndWindow01_1.createSth;
 
 public class MultiInert02 {
+
+    static TypeInformation<?>[] types01 = new TypeInformation[4];
+    static String[] fieldNames01 = new String[4];
+    static RowTypeInfo rowTypeInfo01 = new RowTypeInfo();
+
+    static TypeInformation<?>[] types02 = new TypeInformation[2];
+    static String[] fieldNames02 = new String[2];
+    static RowTypeInfo rowTypeInfo02 = new RowTypeInfo();
+
+    static TypeInformation<?>[] types03 = new TypeInformation[2];
+    static String[] fieldNames03 = new String[2];
+    static RowTypeInfo rowTypeInfo03 = new RowTypeInfo();
+
+    static {
+        types01[0] = TypeInformation.of(new TypeHint<String>() {
+        });
+        types01[1] = TypeInformation.of(new TypeHint<Long>() {
+        });
+        types01[2] = TypeInformation.of(new TypeHint<Long>() {
+        });
+        types01[3] = TypeInformation.of(new TypeHint<Boolean>() {
+        });
+        fieldNames01[0] = "sec_code";
+        fieldNames01[1] = "order_type";
+        fieldNames01[2] = "order_no";
+        fieldNames01[3] = "is_acc";
+        rowTypeInfo01 = new RowTypeInfo(types01, fieldNames01);
+
+
+        types02[0] = TypeInformation.of(new TypeHint<String>() {
+        });
+        types02[1] = TypeInformation.of(new TypeHint<Long>() {
+        });
+        fieldNames02[0] = "sec_code";
+        fieldNames02[1] = "order_type";
+        rowTypeInfo02 = new RowTypeInfo(types02, fieldNames02);
+
+
+        types03[0] = TypeInformation.of(new TypeHint<String>() {
+        });
+        types03[1] = TypeInformation.of(new TypeHint<Long>() {
+        });
+        fieldNames03[0] = "sec_code";
+        fieldNames03[1] = "order_no";
+        rowTypeInfo03 = new RowTypeInfo(types03, fieldNames03);
+    }
+
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
@@ -31,27 +82,39 @@ public class MultiInert02 {
                 "select o.sec_code, o.order_type, c.order_no " +
                         "from kafka_stock_order o left join kafka_stock_order_confirm c on o.order_no = c.order_no");
 
-        TypeInformation<?>[] types = new TypeInformation[3];
-        String[] fieldNames = new String[3];
-        types[0] = TypeInformation.of(new TypeHint<String>() {
-        });
-        types[1] = TypeInformation.of(new TypeHint<Long>() {
-        });
-        types[2] = TypeInformation.of(new TypeHint<Long>() {
-        });
-        types[3] = TypeInformation.of(new TypeHint<Boolean>() {
-        });
-        fieldNames[0] = "sec_code";
-        fieldNames[1] = "order_type";
-        fieldNames[2] = "order_no";
-        fieldNames[3] = "is_acc";
-        RowTypeInfo rowTypeInfo = new RowTypeInfo(types, fieldNames);
-
         DataStream<Row> ds = tableEnv.toRetractStream(joinedTbl, Row.class)
                 .map(tp2 -> Row.join(tp2.f1, Row.of(tp2.f0)))
-                .returns(rowTypeInfo);
+                .returns(rowTypeInfo01);
 
+        OutputTag<Row> outputTag01 = new OutputTag<Row>("side_output_01", rowTypeInfo02){};
+        OutputTag<Row> outputTag02 = new OutputTag<Row>("side_output_02", rowTypeInfo03){};
 
+        SingleOutputStreamOperator<Row> mainDataStream = ds.process(new ProcessFunction<Row, Row>() {
+            @Override
+            public void processElement(Row row, Context ctx, Collector<Row> out) throws Exception {
+                out.collect(row);
+
+                String secCode = row.getField(0).toString();
+                if ("p1".equalsIgnoreCase(secCode)) {
+                    ctx.output(outputTag01, Row.ofKind(row.getKind(), row.getField(0),
+                            row.getField(1)));
+
+                } else if ("p2".equalsIgnoreCase(secCode)) {
+                    ctx.output(outputTag02, Row.ofKind(row.getKind(), row.getField(0),
+                            row.getField(2)));
+                }
+            }
+        }, rowTypeInfo01);
+//        mainDataStream.print();
+//        streamEnv.execute("a");
+
+        DataStream<Row> ds01 = mainDataStream.getSideOutput(outputTag01);
+        tableEnv.fromDataStream(ds01)
+                .executeInsert("side_output_01");
+
+        DataStream<Row> ds02 = mainDataStream.getSideOutput(outputTag02);
+        tableEnv.fromDataStream(ds02)
+                .executeInsert("side_output_02");
     }
 
 
